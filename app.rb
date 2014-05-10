3cb9525a923813cdbf782afcb4ded36b03b6bf5c
Cristiano Betta <cbetta@paypal.com>
2014-05-10 13:10:50 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,56 @@
+require "sinatra"
+require "braintree"
+require './helpers/pretty_print.rb'
+
+class Application < Sinatra::Base
+  helpers PrettyPrint
+
+  get "/" do
+    configure
+    erb :index
+  end
+
+  post "/process" do
+    configure
+
+    result = Braintree::Customer.create(
+      credit_card: {
+        number: params[:number],
+        cvv: params[:cvv],
+        expiration_date: params[:expiration_date],
+        options: {
+          verify_card: true
+        },
+      },
+      first_name: params[:first_name],
+      last_name: params[:last_name]
+    )
+
+    if result.success?
+      @card = result.customer.credit_cards.first
+
+      result = Braintree::Subscription.create(
+        payment_method_token: @card.token,
+        plan_id: params[:plan]
+      )
+
+      if result.success?
+        @subscription = result.subscription
+        erb :process
+      else
+        "Could not create the subscription"
+      end
+    else
+      "Could not create the customer"
+    end
+  end
+
+  private
+
+  def configure
+    Braintree::Configuration.environment = :sandbox
+    Braintree::Configuration.merchant_id = "ffdqc9fyffn7yn2j"
+    Braintree::Configuration.public_key = "qj65nndbnn6qyjkp"
+    Braintree::Configuration.private_key = "a3de3bb7dddf68ed3c33f4eb6d9579ca"
+  end
+end
---
+++ spec/features/successful_payment_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../spec_helper'
+
+describe 'A successful payment', js: true, type: :request do
+  it 'responds with a success message' do
+    visit '/'
+
+    pending
+ end
+end
