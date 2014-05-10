e6934abd40b99159b00065867905b9b61c8970ee
Cristiano Betta <cbetta@paypal.com>
2014-05-10 16:14:22 +0200

    Now actually selects a plan

--- spec/features/successful_payment_spec.rb
+++ spec/features/successful_payment_spec.rb
@@ -8,11 +8,12 @@ describe 'A successful payment', js: true, type: :request do
     fill_in 'number', with: card_number
     fill_in 'cvv', with: '123'
     fill_in 'expiration_date', with: '11/2015'
+    select('Test plan 2 ($30/month)', from: 'plan')
 
     find_button('Submit').click
 
     expect(page).to have_content('Payment Success')
     expect(page).not_to have_content(card_number)
-    expect(page).to have_content('plan_1')
+    expect(page).to have_content('plan_2')
    end
 end
